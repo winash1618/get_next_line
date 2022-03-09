8a4e16fd3d3d52469757e9ce6e4d941e24c3ae73
asharaf <51042907+winash1618@users.noreply.github.com>
2022-03-09 08:08:43 +0400

    corrected error

--- get_next_line.c
+++ get_next_line.c
@@ -6,53 +6,47 @@
 /*   By: mkaruvan <mkaruvan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 13:22:31 by mkaruvan          #+#    #+#             */
-/*   Updated: 2022/03/08 15:42:27 by mkaruvan         ###   ########.fr       */
+/*   Updated: 2022/03/09 08:07:43 by mkaruvan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int jgetter(char *str)
+int	jgetter(char *str)
 {
-	int j;
-
-	j = 0;
 	if (*str == '\n' || *str == '\0')
-		j = 1;
-	return (j);
+		return (1);
 }
 
-char *ft_free(char *str, char *s)
+char	*ft_free(char *str, char *s)
 {
 	free (s);
 	free (str);
 	return (NULL);
 }
 
-char *ft_reader(int fd, char *s)
+char	*ft_reader(int fd, char *s)
 {
 	char	*str;
-	int		i;
-	int		j;
-	int		k;
+	int		i[3];
 
-	j = 0;
-	i = 0;
-	while (j == 0)
+	i[0] = 0;
+	i[1] = 0;
+	while (i[1] == 0)
 	{
-		k = 0;
+		i[2] = 0;
 		str = (char *)malloc(2);
 		if (!str)
 			return (NULL);
 		str[1] = '\0';
-		k = read(fd, str, 1);
-		i += k;
-		if (k == -1)
+		i[2] = read(fd, str, 1);
+		i[0] += i[2];
+		if (i[2] == -1)
 			return (ft_free(str, s));
-		j = jgetter(str);
-		if(k != 0)
+		i[1] = jgetter(str);
+		if (i[2] != 0)
 			s = ft_strjoin(s, str);
-		if (k == 0 && i == 0)
+		if (i[2] == 0 && i[0] == 0)
 			return (ft_free(str, s));
 		free(str);
 	}
@@ -73,11 +67,11 @@ char	*ft_getcline(char *str)
 	return (s);
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-	static char *str;
-	char *s;
-	int i;
+	static char	*str;
+	char		*s;
+	int			i;
 
 	i = 0;
 	if (BUFFER_SIZE <= 0 || fd < 0)
@@ -86,7 +80,7 @@ char *get_next_line(int fd)
 	s = ft_getcline(str);
 	if (!str)
 		return (s);
-	while(str[i])
+	while (str[i])
 		str[i++] = '\0';
 	return (s);
 }
